6fb27db78905ef12aeefe462f8f972708ab27b62
liarliarpantsontyler <116979438+liarliarpantsontyler@users.noreply.github.com>
2025-09-26 13:54:32 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'mono': ['JetBrains Mono', 'Courier New', 'monospace'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
